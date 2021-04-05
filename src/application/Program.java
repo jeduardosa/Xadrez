12d01c0359b139b49cd0870e74bb2716aad56d40
eduardomegha <jeduardosa86@gmail.com>
2021-04-05 12:07:34 -0300

    Correção - starting to implement board and Piece class

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,14 +2,14 @@ package application;
 
 import java.awt.Point;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
-
+		
+		Board board = new Board(8,8);
 	}
 
 }
